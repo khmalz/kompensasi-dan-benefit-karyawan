1555df0e36b757c68462b6ffbae2a113ac8c264d
khmalz <akmalrma1710@gmail.com>
2022-10-08 20:25:31 +0700

    Ketinggalan

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -36,7 +36,7 @@
             <button data-bs-toggle="modal" data-bs-target="#exampleModal" class="nav-link w-100 bg-success d-flex align-items-center justify-content-between border-0 text-success py-1 px-3 rounded-3"  style="--bs-bg-opacity: .3;" >
                <div>
                   <i class="bi bi-bell"></i>
-                  Notifications
+                  Notifikasi
                </div>
                @if (auth()->user()->unreadnotifications->isNotEmpty())
                   <span class="badge text-bg-success rounded-pill me-3 me-md-0" style="--bs-bg-opacity: 1; font-size: .68rem !important">{{ auth()->user()->unreadnotifications->count() }}</span>
@@ -52,24 +52,28 @@
    <div class="modal-dialog modal-dialog-centered">
      <div class="modal-content">
        <div class="modal-header justify-content-center">
-         <h1 class="modal-title fs-5" id="exampleModalLabel">Notifications</h1>
+         <h1 class="modal-title fs-5" id="exampleModalLabel">Notifikasi</h1>
        </div>
        <div class="modal-body">
-         @foreach (auth()->user()->notifications as $notification)
-         <div class="row g-0 mb-4">
-            <div class="alert alert-success d-flex align-items-center justify-content-between" role="alert">
-               <div>
-                  @if ($notification->data['status'] == 'sedang')
-                  <span>Tunjangan dengan kode <span class="fw-semibold">{{ $notification->data['kode'] }}</span> sedang dalam proses</span>
-                  @else
-                  <span>Tunjangan dengan kode <span class="fw-semibold">{{ $notification->data['kode'] }}</span> sudah selesai diproses</span>
-                  @endif
-                  <a href="/tunjangan/{{ $notification->data['kode'] }}" class="text-dark text-decoration-none fw-semibold">Lihat Selengkapnya</a> 
-                  <small><span class="text-secondary d-block mb-1">{{ $notification->created_at->isoFormat('D MMMM, Y') }} pada {{ $notification->created_at->format('H:i') }} | {{ $notification->created_at->diffForHumans() }}</span></small>
+         @if (auth()->user()->notifications->isNotEmpty())
+            @foreach (auth()->user()->notifications as $notification)
+            <div class="row g-0 mb-4">
+               <div class="alert alert-success d-flex align-items-center justify-content-between" role="alert">
+                  <div>
+                     @if ($notification->data['status'] == 'sedang')
+                     <span>Tunjangan dengan kode <span class="fw-semibold">{{ $notification->data['kode'] }}</span> sedang dalam proses</span>
+                     @else
+                     <span>Tunjangan dengan kode <span class="fw-semibold">{{ $notification->data['kode'] }}</span> sudah selesai diproses</span>
+                     @endif
+                     <a href="/tunjangan/{{ $notification->data['kode'] }}" class="text-dark text-decoration-none fw-semibold">Lihat Selengkapnya</a> 
+                     <small><span class="text-secondary d-block mb-1">{{ $notification->created_at->isoFormat('D MMMM, Y') }} pada {{ $notification->created_at->format('H:i') }} | {{ $notification->created_at->diffForHumans() }}</span></small>
+                  </div>
                </div>
-             </div>
-         </div>
-         @endforeach
+            </div>
+            @endforeach
+         @else
+         <div class="d-flex justify-content-center"><span class="text-secondary">Tidak Ada Notifikasi</span></div>
+         @endif
        </div>
      </div>
    </div>
